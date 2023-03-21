cabe70e2a2ac97d1b21ee6ef2cb165c6fbb41e89
igede_yogaputra <igede_yogaputra@bca.co.id>
2023-03-22 01:36:17 +0700

    Test-3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 @RestController
@@ -16,12 +19,72 @@ public class DemoApplication {
     }
 
     @RequestMapping("/test1234")
-    String home() {
+    String home1() {
         return "Test1234!";
+    }
+
+    @RequestMapping("/test456")
+    String home2() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("sh", "-c", "curl -v  https://www.google.com");
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+            return "Success";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed!";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "Failed!"
+        }
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+
+
+
+public class ProcessBuilderExample1 {
+
+    public static void main(String[] args) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        // Windows
+        processBuilder.command("cmd.exe", "/c", "ping -n 3 google.com");
+
+        try {
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
